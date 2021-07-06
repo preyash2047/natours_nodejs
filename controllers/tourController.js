adb6d08e5ed86af2b102d29ad67e26aaec0f8211
Preyash Patel <preyash2047@GMAIL.com>
2021-07-06 18:38:20 +0530

    24th done

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,9 +4,10 @@ const APIFeatures = require('../utils/apiFeatures.js')
 exports.aliesTopTours = async (req, res, next) =>{
     req.query.limit = 5;
     req.query.sort = "price";
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
     next();
 }
-
+ 
 //getTours
 exports.getTours = async (req, res) =>{
     try{
@@ -112,14 +113,23 @@ exports.getTourStats = async (req, res) => {
         const stats = await Tour.aggregate([
             {
                 $match: { ratingsAverage: { $gte : 4.5 } }
-            },
+            },  
             {
                 $group: {
-                    _id : null,
-                    averageRating: { $avg : '$ratingsAverage'},
+                    _id : {$toUpper : "$difficulty"},//"$ratingsAverage",
+                    numTures: {$sum:1},
+                    numRatings: {$sum:'$ratingsQuantity'},
+                    avgRating: { $avg : '$ratingsAverage'},
                     avgPrice : { $avg : '$price'},
-                    minPrice : { $min : '$price'}
+                    minPrice : { $min : '$price'},
+                    maxPrice : { $max : '$price'}
                 }
+            },
+            {
+                $sort: { avgPrice: 1}
+            },
+            {
+                $match: { _id:{ $ne :"EASY"}}
             }
         ]);
 
@@ -138,14 +148,42 @@ exports.getTourStats = async (req, res) => {
     }
 }
 
+//ERROR
 exports.getMonthlyPlan = async (req, res) =>{
     try {
         const year = req.params.year * 1;
         const plan = await Tour.aggregate([
-           {
-                $unwind : "$startDate"
-           }
-        ]);
+            {
+              $unwind: '$startDates'
+            },
+            {
+              $match: {
+                startDates: {
+                  $gte: new Date(`${year}-01-01`),
+                  $lte: new Date(`${year}-12-31`)
+                }
+              }
+            },
+            {
+              $group: {
+                _id: { $month: '$startDates' },
+                numTourStarts: { $sum: 1 },
+                tours: { $push: '$name' }
+              }
+            },
+            {
+              $addFields: { month: '$_id' }
+            },
+            {
+              $project: { _id: 0 }
+            },
+            {
+              $sort: { numTourStarts: -1 }
+            },
+            {
+              $limit: 12
+            }
+          ]);
 
         res.status(200).json({
             status: "success",
